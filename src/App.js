02c34a2f597271e3d490319fbe34d7beca4d7952
Jebakumar <jebakumars@blackant.io>
2022-03-20 18:52:09 +0530

    Implement the dropdowns based on json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import fetchQuote from './fetchQuote';
+// import testjson from './test_qf.json';
 import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
+  const [receviedJson, setReceivedJson] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [adp, setAdp] = useState([]);
+  
+
+  const getJsonData = async () => {
+    const { data } = await axios.get('./test_qf.json');
+    setReceivedJson(Object.assign([], data));
+  };
+
+  
+  useEffect(()=>{
+    getJsonData();
+  },[])
+
+  function populateUsers(asid) {
+    console.log("selected asn", asid);
+      receviedJson.map((asn) => {
+        if(asn.asid === asid) {
+          setUsers(Object.assign([], asn.unit));
+          
+        }
+      })     
+  }
+
+  function populateAdp(userid) {
+      users.map((user) => {
+        if(user.uid === userid) {
+          setAdp(Object.assign([], user.ap));
+          
+        }
+      })
+  }
+  
+   return (
+     
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <label></label>   
+        <select className="dropdown" onChange={(event)=>{ populateUsers(event.target.value)}}>
+          {receviedJson.map((asn) => { 
+          return(
+              <option key={asn.asid} value={asn.asid}>{asn.asn}</option>
+          )})}            
+        </select>
+        {(users.length > 0) ? 
+        <select className="dropdown" onChange={(event)=>{ populateAdp(event.target.value)}}>
+          {users.map((user) => { 
+          return(
+              <option key={user.uid} value={user.uid}>{user.uname}</option>
+          )})}            
+        </select>
+        : "" }
+        {(adp.length > 0) ? 
+          <select className="dropdown">
+            {adp.map((adp) => { 
+            return(
+                <option key={adp.adpid} value={adp.adpid}>{adp.adp}</option>
+            )})}            
+          </select> 
+        : "" }
+        
     </div>
+
   );
 }
 
